27a13408bad5fc961613ebbed67e9e5f2e3fb914
Dominic Chambers <dominic.chambers@gmail.com>
2014-10-16 20:06:39 +0100

    Updated CrosstalkJs to use the same shared build code as IterizerJs.

--- scripts/prepublish.js
+++ scripts/prepublish.js
@@ -1,25 +1,6 @@
-var fs = require('fs');
-var mkdirp = require('mkdirp');
-var browserify = require('browserify');
+var build = require('dchambers-lib-build-tool');
 
-mkdirp('dist/mocha', function(err) {
-	if(err) {
-		console.error(err);
-	}
-	else {
-		createBundle('dist/crosstalk.js', ['./lib/global-crosstalk.js']);
-		createBundle('dist/crosstalk-spec-tests.js', ['./spec/test/tests.spec.js']);
-
-		fs.createReadStream('node_modules/mocha/mocha.js').pipe(fs.createWriteStream('dist/mocha/mocha.js'));
-		fs.createReadStream('node_modules/mocha/mocha.css').pipe(fs.createWriteStream('dist/mocha/mocha.css'));
-	}
-});
-
-function createBundle(outputFilePath, inputFilePaths) {
-	var b = browserify();
-	for(var key in inputFilePaths) {
-		b.add(inputFilePaths[key]);
-	}
-	
-	b.bundle().pipe(fs.createWriteStream(outputFilePath));
-}
+build.bundleDeps(
+	build.bundle('crosstalk.js', ['./lib/global-crosstalk.js']),
+	build.bundle('crosstalk-spec-tests.js', ['./spec/test/tests.spec.js'])
+);
